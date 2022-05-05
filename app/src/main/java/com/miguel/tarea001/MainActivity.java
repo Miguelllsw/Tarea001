f60002f9e6b406157e3e3e763d8d6e928cb716f8
migue <miguelhernandez.gonzalez28@gmail.com>
2022-05-04 20:41:38 -0400

    Tarea001/04-05-22------Parte2

--- app/src/main/java/com/miguel/tarea001/MainActivity.java
+++ app/src/main/java/com/miguel/tarea001/MainActivity.java
@@ -3,12 +3,51 @@ package com.miguel.tarea001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView  tvResultado;
+    private EditText etNumero1, etNumero2;
+    private Button btSuma, btMultiplicar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvResultado = findViewById(R.id.tvResultado);
+        etNumero1 = findViewById(R.id.etNumero1);
+        etNumero2 = findViewById(R.id.etNumero2);
     }
+    public void suma(View view){
+        try{
+            int valor1 = Integer.parseInt(etNumero1.getText().toString());
+            int valor2 = Integer.parseInt(etNumero2.getText().toString());
+
+            tvResultado.setText(String.valueOf("Resultado: "+(valor1 + valor2)));
+
+        }catch (NumberFormatException exception){
+            tvResultado.setText("Error");
+        }
+
+
+
+    }
+    public void multiplicacion(View view){
+        try{
+            int valor1 = Integer.parseInt(etNumero1.getText().toString());
+            int valor2 = Integer.parseInt(etNumero2.getText().toString());
+            tvResultado.setText(String.valueOf("Resultado: "+valor1 * valor2));
+
+        }catch (NumberFormatException exception){
+            tvResultado.setText("Error");
+        }
+
+
+    }
+
 }
